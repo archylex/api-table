82486c6886da25cac9cf59e1be19f32335346442
archylex <arhylexx@gmail.com>
2022-09-26 17:04:07 +0300

    Starting

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express') ;
+const app = express();
+
+function requestListener(req, res) {
+    console.log('hey');
+}
+
+function getRoot(req, res) {
+    res.send('Api');
+}
+
+function getPost(req, res) {
+    res.json(posts);
+}
+
+app.get('/', getRoot);
+app.get('/posts', getPosts);
+
+app.listen(process.env.PORT || 3000, requestListener);
